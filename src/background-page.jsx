db78dda92825461a1e18e9f359f30806ee477e02
Wis <im@wis.am>
2020-03-11 03:49:20 +0200

    rename background-page.js to .jsx

--- src/background-page.jsx
+++ src/background-page.jsx
@@ -1,15 +1,11 @@
 import * as React from "react";
 import { render } from "react-dom";
-import { Componentx } from "./componentx";
 import Componentxjs from "./componentxjs";
-import { ClassComponentx } from "./classcomponentx";
 
 class Background extends React.Component {
   render() {
     return (
       <>
-        <Componentx>popup</Componentx>
-        <ClassComponentx>popuuup</ClassComponentx>
         <Componentxjs>popup</Componentxjs>
       </>
     );
